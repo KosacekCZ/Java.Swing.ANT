65304df4bbf4952868c0e8959016c55f2e2e570c
Mikoláš Pešek <35407839+KosacekCZ@users.noreply.github.com>
2022-07-17 22:27:25 +0200

    ver. 3.0

--- src/Frame.java
+++ src/Frame.java
@@ -1,117 +1,157 @@
 import javax.swing.*;
 
 import java.awt.*;
+import java.util.HashMap;
 
 public class Frame {
 
-    int width = 50;
-    int height = 50;
-    int delay = 0;
+    int width = 200;
+    int height = 200;
+    int res = 4;
+    int startDelay = 15000;
+    int genDelay = 0;
+    int startNoise = ((width * height) / 4);
 
     JFrame fr = new JFrame("ANT");
     JButton jb;
-    Square[][] field = new Square[width][height];
+    // Square[][] field = new Square[width][height];
+    HashMap<String, Square> field = new HashMap<>();
+
 
     void initField() {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                field[i][j] = (new Square(i, j, false, new JButton()));
-                field[i][j].jb.setBounds(i * 10, j * 10, 10, 10);
-                field[i][j].jb.setBackground(Color.black);
-                field[i][j].jb.setBorderPainted(false);
-                fr.add(field[i][j].jb);
-                System.out.println(i + " " + j);
+                field.put(i + " " + j, (new Square(i, j, false, new JButton(), 0)));
+                field.get(i + " " + j).jb.setBounds(i * res, j * res, res, res);
+                field.get(i + " " + j).jb.setBackground(Color.black);
+                field.get(i + " " + j).jb.setBorderPainted(false);
+                fr.add(field.get(i + " " + j).jb);
+                // System.out.println(i + " " + j);
             }
         }
     }
-
 
     void initFrame() throws InterruptedException {
         fr.setLayout(new GridLayout(width, height));
-        fr.setSize((width + 1) * 10, (height + 1) * 10);
+        fr.setSize((width) * res, (height) * res);
         fr.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        fr.setResizable(false);
+        fr.setResizable(true);
         fr.getContentPane().setBackground(Color.black);
         initField();
         fr.setVisible(true);
-        for (int i = 0; i < 10; i++) {
-            start();
-        }
-        scan();
-        while (true) {
-            spread();
-        }
+        start();
     }
 
-    void start() {
-        int rx = randRange(0, width);
-        int ry = randRange(0, height);
-        field[rx][ry].flipped = true;
-        // gen sqr
-        try {
-            for (int i = -1; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    field[rx + i][ry + j].flipped = true;
+    void calcNb() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int nbCount;
+                if (field.get(i + " " + j).alive) {
+                    nbCount = -1;
+                } else {
+                    nbCount = 0;
                 }
-            }
-        } catch (Exception e ) {
-            // System.out.println("border");
-        }
-         System.out.println("start");
-    }
 
-    void scan() {
-        for (Square[] col : field) {
-            for (Square sqr : col) {
-                if (sqr.flipped) {
-                    int x = sqr.x;
-                    int y = sqr.y;
+                // check neighbour cells
+                for (int k = -1; k <= 1; k++) {
+                    for (int l = -1; l <= 1; l++) {
+                        String currentSqr = (i + k) + " " + (j + l);
+                        Square key = field.get(currentSqr);
+                        if (key != null) {
 
-                    field[x][y].jb.setBackground(Color.yellow);
-                    fr.revalidate();
-
-                }
-            }
-        }
-    }
-
-    void spread() throws InterruptedException {
-
-        for (Square[] col : field) {
-            for (Square sqr : col) {
-                Thread.sleep(delay);
-                // check cell
-                int x = sqr.x;
-                int y = sqr.y;
-                int nbCount = 0;
-                    // field[x][y].jb.setBorderPainted(true);
-                    // field[x][y].jb.setBorder(BorderFactory.createLineBorder(Color.green));
-                    // fr.revalidate();
-                // System.out.println("Checking " + x + " | " + y);
-                for (int i = -1; i <= 1; i++) {
-                    for (int j = -1; j <= 1; j++) {
-                        try {
-                            if (field[x + i][y + j].flipped) {
+                            if (field.get(currentSqr).alive) {
                                 nbCount++;
-
                             }
-                        } catch (Exception e) {
-                            // System.out.println("border");
                         }
+                        field.get(i + " " + j).nb = nbCount;
+
                     }
                 }
-                if (nbCount >= 3) {
-                    field[x][y].flipped = true;
-                }
-                scan();
-
+                // System.out.println("cell " + i + " " + j + " has " + nbCount + " neighbours");
             }
+
+                /* if (field.get(i + " " + j).alive) {
+                    if (nbCount < 2) {
+                        field.get(i + " " + j).alive = false;
+                        field.get(i + " " + j).jb.setBackground(Color.black);
+                        System.out.println("cell with 2 or less neighbours has died.");
+                    } else if (nbCount == 2 || nbCount == 3) {
+                        field.get(i + " " + j).alive = true;
+                        field.get(i + " " + j).jb.setBackground(Color.yellow);
+                    } else if (nbCount > 3) {
+                        field.get(i + " " + j).alive = false;
+                        field.get(i + " " + j).jb.setBackground(Color.black);
+                        System.out.println("cell with 4 or more neighbours has died .");
+                    }
+                } else {
+                    // dead cell
+                    if (nbCount == 3) {
+                        field.get(i + " " + j).alive = true;
+                        field.get(i + " " + j).jb.setBackground(Color.yellow);
+                    }
+               }
+                    */
         }
     }
 
-    public int randRange(int min, int max) {
-        return (int) ((Math.random() * (max - min)) + min);
+    void nextGen() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (field.get(i + " " + j).alive) {
+                    switch (field.get(i + " " + j).nb) {
+                        case 0:
+                        case 1:
+                            field.get(i + " " + j).alive = false;
+                            field.get(i + " " + j).jb.setBackground(Color.black);
+                            // System.out.println("cell with 2 or less neighbours has died.");
+                            break;
+                        case 2:
+                        case 3:
+                            field.get(i + " " + j).alive = true;
+                            field.get(i + " " + j).jb.setBackground(Color.yellow);
+                            break;
+                        case 4:
+                        case 5:
+                        case 6:
+                        case 7:
+                        case 8:
+                            field.get(i + " " + j).alive = false;
+                            field.get(i + " " + j).jb.setBackground(Color.black);
+                            // System.out.println("cell with 4 or more neighbours has died .");
+                            break;
+                    }
+                } else {
+                    if (field.get(i + " " + j).nb == 3) {
+                        field.get(i + " " + j).alive = true;
+                        field.get(i + " " + j).jb.setBackground(Color.yellow);
+                    }
+                }
+            }
+        }
+        fr.validate();
     }
 
-}
+    void spawnNoise() {
+        for (int i = 0; i < startNoise; i++) {
+            int rx = randRange(0, width);
+            int ry = randRange(0, height);
+            field.get(rx + " " + ry).alive = true;
+            field.get(rx + " " + ry).jb.setBackground(Color.yellow);
+        }
+        fr.validate();
+    }
 
+    void start() throws InterruptedException {
+        spawnNoise();
+        Thread.sleep(startDelay);
+        while (true) {
+            Thread.sleep(genDelay);
+            calcNb();
+            nextGen();
+        }
+    }
+
+    int randRange(int min, int max) {
+        return (int) ((Math.random() * (max - min)) + min);
+    }
+}
--- src/Square.java
+++ src/Square.java
@@ -3,14 +3,17 @@ import javax.swing.*;
 public class Square {
     int x;
     int y;
-    boolean flipped;
+    int nb;
+    boolean alive;
     JButton jb;
 
-    public Square(int x, int y, boolean flipped, JButton jb) {
+    public Square(int x, int y, boolean alive, JButton jb, int nb) {
         this.x = x;
         this.y = y;
-        this.flipped = flipped;
+        this.nb = nb;
+        this.alive = alive;
         this.jb = jb;
+
     }
 
 }
